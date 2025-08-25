1be6a3336e3afbbe5cd61daedc26200eca03c7dc
SOURCE-CODE <143252411+ADERICHTI@users.noreply.github.com>
2025-08-25 09:19:43 +0100

    Update settings.py

--- iarap/iarap/settings.py
+++ iarap/iarap/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     # 'corsheaders.middleware.CorsMiddleware'
+    'firestore_api.cors_middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -153,5 +154,6 @@ REST_FRAMEWORK = {
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
+
 
 
